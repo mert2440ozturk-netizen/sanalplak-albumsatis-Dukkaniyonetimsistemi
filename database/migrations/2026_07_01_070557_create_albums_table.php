46ba0c8364726266de135a239f28c8b5bb6f89c6
Mert <mert2440ozturk@gmail.com>
2026-07-06 11:58:02 +0300

    Initial Laravel setup with database migrations
    Added base Laravel configuration files, public entry files, and initial database migration structure for the project.

---
+++ database/migrations/2026_07_01_070557_create_albums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+
+            $table->foreignId('artist_id')->constrained();
+            $table->foreignId('genre_id')->constrained();
+
+            $table->integer('release_year');
+            $table->integer('stock');
+
+            $table->decimal('price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
